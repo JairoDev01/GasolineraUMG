fb350db17664919d5a766646d14182485b2cb103
Jairo Velásquez <jairo.j5180@gmail.com>
2018-10-19 22:56:01 -0600

    Creación del proyecto

---
+++ src/gasolinera/Menu.java
@@ -0,0 +1,17 @@
+
+package gasolinera;
+
+/**
+ *
+ * @author JairoDev
+ */
+public class Menu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
